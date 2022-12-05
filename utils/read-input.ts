7a9d62fc2bd08e8672fb978f6b3ca851e88d7691
Joris Molnar <joris.molnar@gmail.com>
2022-12-06 00:02:37 +0100

    fix readInput trimming spaces

--- utils/read-input.ts
+++ utils/read-input.ts
@@ -1,4 +1,5 @@
 import { readFile } from 'fs/promises'
+import { trimEnd } from 'lodash'
 
 /** Read input.txt, assuming it is in current working directory. */
 export async function readInput (part: string): Promise<string> {
@@ -19,7 +20,7 @@ export async function readExampleInput (part: string): Promise<string> {
 async function read (path: string): Promise<string> {
   try {
     const contents = await readFile(path, 'utf-8')
-    return contents.trim()
+    return trimEnd(contents, '\n')
   } catch (err) {
     console.error(`Could not read ${path}. Did you forget to create it?`)
     throw new Error(`Could not read ${path}`)
